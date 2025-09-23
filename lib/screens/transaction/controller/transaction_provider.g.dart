e74c7ddfb95424eca85f412ea98bd83a8fc17913
DuongNA <duongna@ltsgroup.tech>
2025-09-23 22:02:52 +0700

    Add transaction provider.g.dart

---
+++ lib/screens/transaction/controller/transaction_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'transaction_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$transactionNotifierHash() =>
+    r'5d0d31b5acad1ec6abb46cd712f4ba2612685caa';
+
+/// See also [TransactionNotifier].
+@ProviderFor(TransactionNotifier)
+final transactionNotifierProvider =
+    AutoDisposeNotifierProvider<TransactionNotifier, int>.internal(
+  TransactionNotifier.new,
+  name: r'transactionNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$transactionNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TransactionNotifier = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
